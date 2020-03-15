c4a7f647da4896c04a3cc523cda24fffb8b3a607
Tharun Medini <tharunkumarreddy24@gmail.com>
2020-03-14 20:50:39 -0500

    fixed bugs in sampled softmax code

--- python_examples/config.py
+++ python_examples/config.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class config:
-    data_path_train = 'data/amazon-670K/amazon_train.txt'
-    data_path_test = 'data/amazon-670K/amazon_test.txt'
-    GPUs = '' # empty string uses only CPU
+    data_path_train = '/home/bc20/NN/data/amazon_shuf_train'
+    data_path_test = '/home/bc20/NN/data/amazon_shuf_test'
+    GPUs = '7' # empty string uses only CPU
     num_threads = 44 # Only used when GPUs is empty string
     lr = 0.0001
     ###
@@ -11,12 +11,14 @@ class config:
     n_classes = 670091
     n_train = 490449
     n_test = 153025
-    n_epochs = 20
+    n_epochs = 2
     batch_size = 128
     hidden_dim = 128
     ###
-    log_file = 'log'
-
+    log_file = 'log_amz_ss'
     ### for sampled softmax
-    n_samples = 670091//10
-    max_label = 100
+    n_samples = n_classes//10
+    ### choose the max_labels per training sample. 
+    ### If the number of true labels is < max_label,
+    ### we will pad the rest of them with a dummy class (see data_generator_ss in util.py)
+    max_label = 1
