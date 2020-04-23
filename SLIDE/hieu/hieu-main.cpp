44d777b7e68d97039613d75838a1a26e0d92279b
Hieu Hoang <hieuhoang@gmail.com>
2020-04-23 18:08:47 +0000

    start fork

--- SLIDE/hieu/hieu-main.cpp
+++ SLIDE/hieu/hieu-main.cpp
@@ -1,12 +1,21 @@
 #include "hieu-main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 namespace hieu {
 int main(int argc, char *argv[]) {
+  cerr << "Starting" << endl;
   size_t numEpochs = 5;
 
   for (size_t epoch = 0; epoch < numEpochs; epoch++) {
   }
 
+
+
+  cerr << "Finished" << endl;
   exit(0);
 }
 } // namespace hieu
